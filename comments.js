c3c23b9dccc4cb296d25a906962e92c475263cb3
Torbjørn Bratland <45685952+tobratland@users.noreply.github.com>
2023-12-03 16:14:51 +0000

    added comments.js

---
+++ comments.js
@@ -0,0 +1,15 @@
+//create web server
+//create a route for /comments
+//send back some dummy data
+
+import { createServer } from 'http';
+import { createReadStream } from 'fs';
+
+var server = createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = createReadStream(__dirname + '/comments.json', 'utf8');
+    myReadStream.pipe(res);
+});
+
+server.listen(3000, "localhost");
