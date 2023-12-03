8e537700ce530bd5161b8ddd8cae42bc7f235f25
Torbjørn Bratland <45685952+tobratland@users.noreply.github.com>
2023-12-03 16:08:50 +0000

    something cool!

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsmember() {
+    var member = document.getElementsByName("member");
+    var skill = document.getElementsByName("skill");
+    var memberValue = member[0].value;
+    var skillValue = skill[0].value;
+    var memberSkills = memberValue + " " + skillValue;
+    var memberSkillsElement = document.getElementById("memberSkills");
+    memberSkillsElement.innerHTML = memberSkills;
+}
